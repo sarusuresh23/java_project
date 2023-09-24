308ab4cd4d1bfb7ab4af7cd2c330341aa6b4a15c
sarus <sarus@saru>
2023-09-24 20:22:48 +0530

    ex1 uploaded

---
+++ java1/src/java1/typecasting.java
@@ -0,0 +1,21 @@
+package java1;
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		//implicit typecasting
+		int a=10;
+		float b=a;
+		System.out.println(b);
+		//explicit typecasting
+		char c='a';
+		int d=(int)c;
+		System.out.println(d);
+
+		
+		
+		
+
+	}
+
+}
