ee612ab54fa0ba76ef4f8f116ca8180d5e4a4579
sarus <sarus@saru>
2023-09-25 18:51:53 +0530

    task

--- java1/src/java1/Addanddisplay.java
+++ java1/src/java1/Addanddisplay.java
@@ -13,8 +13,6 @@ public class Addanddisplay {
 	            }
 	        }
 	    }
-
-	    // Function to calculate the sum of digits in a number
 	    public static int get(int num) {
 	        int sum = 0;
 	        while (num != 0) {
